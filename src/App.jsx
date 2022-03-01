0bbd3eb3bd3c7744ae5fc5686d76bcaec34557e4
krishna <keshri819@gmail.com>
2022-03-01 16:11:46 +0530

    All Routing Done in Movie App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 import "./App.scss";
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
+import { MovieDetail } from "./components/MovieDetail/MovieDetail";
+import { Footer } from "./components/Footer/Footer";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+console.log("Route:", Route);
+console.log("Routes:", Routes);
 
 export const App = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <>
+      <div className="app">
+        <BrowserRouter>
+          <Header></Header>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/movie/:imdbId" element={<MovieDetail />}></Route>
+            <Route path="*" element={<h1>Nott Found</h1>}></Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </>
+  );
 };
