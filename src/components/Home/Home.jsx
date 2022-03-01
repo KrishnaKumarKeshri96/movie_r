d35280ae4609185ed9f6f628aa74bf145c8e7ecd
krishna <keshri819@gmail.com>
2022-03-01 23:46:04 +0530

    Redux Devtool Set

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,16 +3,19 @@ import { useEffect } from "react";
 import { MovieListing } from "../MovieListing/MovieListing";
 import { fetchApi } from "../../common/apis/movieApi";
 import { APIKEY } from "../../common/apis/movieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 export const Home = () => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const fetchMovie = async () => {
-      const response = await fetchApi
+      await fetchApi
         .get(`?apiKey=${APIKEY}&s=Harry&type=movie`)
-        .then((success) => console.log(success))
+        .then((success) => dispatch(addMovies(success.data)))
 
         .catch((error) => console.log(error));
-      // console.log(response);
     };
     fetchMovie();
   }, []);
--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../features/movies/movieSlice";
 
 export const MovieListing = () => {
+  const allMovie = useSelector(getAllMovies);
+
   return <div>MovieListing</div>;
 };
