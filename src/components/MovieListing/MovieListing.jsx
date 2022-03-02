7f52e461720f430571636c2ac1f4468c9d80ec60
krishna <keshri819@gmail.com>
2022-03-03 01:33:00 +0530

    Added Carasoul in HOme Page

--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { getAllMovies, getAllSeries } from "../../features/movies/movieSlice";
 import { MovieCard } from "../MovieCard/MovieCard";
+import Slider from "react-slick";
 import "./MovieListing.scss";
+import { settings } from "./Setting";
+
 export const MovieListing = () => {
   const allMovie = useSelector(getAllMovies);
   const allShows = useSelector(getAllSeries);
@@ -13,21 +16,25 @@ export const MovieListing = () => {
         <div className="movie-list">
           <h2>Movie List</h2>
           <div className="movie-container">
-            {allMovie.Response === "True"
-              ? allMovie.Search.map((movie, index) => (
-                  <MovieCard key={index} {...movie} />
-                ))
-              : null}
+            <Slider {...settings}>
+              {allMovie.Response === "True"
+                ? allMovie.Search.map((movie, index) => (
+                    <MovieCard key={index} {...movie} />
+                  ))
+                : null}
+            </Slider>
           </div>
         </div>
         <div className="movie-list">
           <h2>Show List</h2>
           <div className="movie-container">
-            {allShows.Response === "True"
-              ? allShows.Search.map((movie, index) => (
-                  <MovieCard key={index} {...movie} />
-                ))
-              : null}
+            <Slider {...settings}>
+              {allShows.Response === "True"
+                ? allShows.Search.map((movie, index) => (
+                    <MovieCard key={index} {...movie} />
+                  ))
+                : null}
+            </Slider>
           </div>
         </div>
       </div>
