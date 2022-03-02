5b85db05b664266c6553505b14f1e532a74924b1
krishna <keshri819@gmail.com>
2022-03-03 01:55:22 +0530

    Added Css for Search BAr

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
 import { Link } from "react-router-dom";
 import user from "../../images/user.png";
 import "./Header.scss";
 
 export const Header = () => {
+  const [searchTern, setsearchTern] = useState("");
   return (
     <>
       <div className="header">
         <Link to="/">
           <div className="logo">FindYourMovie</div>
         </Link>
+
+        <div className="search">
+          <input
+            type="text"
+            value={searchTern}
+            onChange={(e) => {
+              setsearchTern(e.target.value);
+            }}
+            placeholder="Search Movie Or Show"
+          />
+          <button onClick={() => console.log(searchTern)}>
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
 
         <div className="user-image">
           <img src={user} alt="user" />
