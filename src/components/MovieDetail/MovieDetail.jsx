a40667adc78b2a77908f8cf9ccb2693ead6cc1d0
krishna <keshri819@gmail.com>
2022-03-03 00:25:38 +0530

    Show Details Page Done

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import "./MovieDetail.scss";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import {
@@ -10,12 +11,77 @@ import { useSelector } from "react-redux";
 export const MovieDetail = () => {
   const { imdbId } = useParams();
   const dispatch = useDispatch();
-  // const getDetail = useSelector(getDetails);
+  const {
+    Title,
+    Year,
+    Rated,
+    Released,
+    Runtime,
+    Genre,
+    Director,
+    Writer,
+    Actors,
+    imdbRating,
+    imdbVotes,
+    Plot,
+    Language,
+    Awards,
+    Poster,
+  } = useSelector(getDetails);
   // console.log("getDetail:", getDetail);
 
   useEffect(() => {
     dispatch(fetchShowOrMovieDetail(imdbId));
   }, []);
 
-  return <div>MovieDetail</div>;
+  return (
+    <>
+      <div className="movie-section">
+        <div className="section-left">
+          <div className="movie-title">{Title}</div>
+          <div className="movie-rating">
+            <span>
+              IMDB rating <i className="fa fa-star"></i>: {imdbRating}
+            </span>
+            <span>
+              IMDB Votes <i className="fa fa-thumbs-up"></i>: {imdbVotes}
+            </span>
+            <span>
+              Runtime <i className="fa fa-film"></i>: {Runtime}
+            </span>
+            <span>
+              Year <i className="fa fa-calendar"></i>: {Year}
+            </span>
+          </div>
+          <div className="movie-plot">{Plot}</div>
+
+          <div className="movie-info">
+            <div>
+              <span>Director : </span>
+              <span>{Director}</span>
+            </div>
+            <div>
+              <span>Stars : </span>
+              <span>{Actors}</span>
+            </div>
+            <div>
+              <span>Genres : </span>
+              <span>{Genre}</span>
+            </div>
+            <div>
+              <span>Languages : </span>
+              <span>{Language}</span>
+            </div>
+            <div>
+              <span>Awards : </span>
+              <span>{Awards}</span>
+            </div>
+          </div>
+        </div>
+        <div className="section-right">
+          <img src={Poster} alt={Title} />
+        </div>
+      </div>
+    </>
+  );
 };
